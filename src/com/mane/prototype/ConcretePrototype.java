8e8f4bc1ec5653d878a67ca8e59a6a03c3b79c18
Alioune MANE <aliounemane@finetechgroupe.com>
2022-01-02 10:42:19 +0000

    first commit Prototype

--- src/com/mane/prototype/ConcretePrototype.java
+++ src/com/mane/prototype/ConcretePrototype.java
@@ -1,6 +1,10 @@
 package com.mane.prototype;
 
 public class ConcretePrototype extends Prototype {
+
+    /**
+     * Implements Prototype, meaning clone method.
+     */
     @Override
     Prototype copyMe() throws CloneNotSupportedException {
         return (Prototype) this.clone();
